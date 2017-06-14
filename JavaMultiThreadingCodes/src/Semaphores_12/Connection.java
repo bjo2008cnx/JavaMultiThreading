e30e503fc408c9260ecd89126c0f43b43aafdc46
xiaoming.wang <18600882246@163.com>
2017-06-14 22:11:53 +0800

    add comment etc.

--- JavaMultiThreadingCodes/src/Semaphores_12/Connection.java
+++ JavaMultiThreadingCodes/src/Semaphores_12/Connection.java
@@ -2,41 +2,11 @@ package Semaphores_12;
 
 import java.util.concurrent.Semaphore;
 
-/**
- * Semaphores
- * <br><br>
- * Codes with minor comments are from
- * <a href="http://www.caveofprogramming.com/youtube/">
- * <em>http://www.caveofprogramming.com/youtube/</em>
- * </a>
- * <br>
- * also freely available at
- * <a href="https://www.udemy.com/java-multithreading/?couponCode=FREE">
- *     <em>https://www.udemy.com/java-multithreading/?couponCode=FREE</em>
- * </a>
- *
- * @author Z.B. Celik <celik.berkay@gmail.com>
- */
 public class Connection {
 
     private static Connection instance = new Connection();
-/*
-    limit connections to 10
-    true means whichever thread gets first in the waiting pool (queue)
-    waiting to acquire a resource, is first to obtain the permit.
 
-    Note that I called it a pool!
-    The reason is when you say "Queue", you're saying that things are
-    scheduled to be FIFO (First In First Out) .. which is not always the case
-    here!
-    When you initialize the semaphore with Fairness, by setting its second
-    argument to true, it will treat the waiting threads like FIFO.
-    But,
-    it doesn't have to be that way if you don't set on the fairness. the JVM
-    may schedule the waiting threads in some other manner that it sees best
-    (See the Java specifications for that).
-*/
-    private Semaphore sem = new Semaphore(10, true);
+    private Semaphore sem = new Semaphore(5, true);
     private int connections = 0;
 
     private Connection() {
@@ -73,6 +43,7 @@ public class Connection {
             System.out.println("Working on connections " + Thread.currentThread().getName());
             Thread.sleep(2000);
         } catch (InterruptedException ignored) {}
+
         //when exit doConnect method decrement number of connections
         synchronized (this) {//atomic
             connections--;
