8b491e844b6ea6243e44de90b39cf9b2c4d4eec3
xiaoming.wang <18600882246@163.com>
2018-01-12 10:16:47 +0800

    add comment

--- JavaMultiThreadingCodes/src/Atomic/simple/AtomicReferenceTest.java
+++ JavaMultiThreadingCodes/src/Atomic/simple/AtomicReferenceTest.java
@@ -2,12 +2,16 @@ package Atomic.simple;// AtomicReferenceTest.java的源码
 
 import java.util.concurrent.atomic.AtomicReference;
 
+/**
+ * 对象乐观锁
+ */
 public class AtomicReferenceTest {
 
     public static void main(String[] args) {
         // 创建两个Person对象，它们的id分别是101和102。
         Person p1 = new Person(101, "Wang");
         Person p2 = new Person(102, "Li");
+
         // 新建AtomicReference对象，初始化它的值为p1对象
         AtomicReference ar = new AtomicReference(p1);
         // 通过CAS设置ar。如果ar的值为p1的话，则将其设置为p2。
--- JavaMultiThreadingCodes/src/Atomic/stamped/AtomicReferenceTest.java
+++ JavaMultiThreadingCodes/src/Atomic/stamped/AtomicReferenceTest.java
@@ -2,12 +2,16 @@ package Atomic.stamped;// AtomicReferenceTest.java的源码
 
 import java.util.concurrent.atomic.AtomicStampedReference;
 
+/**
+ * 带时间戳的对象无锁测试
+ */
 public class AtomicReferenceTest {
 
     public static void main(String[] args) {
         // 创建两个Person对象，它们的id分别是101和102。
         Person p1 = new Person(101, "Wang");
         Person p2 = new Person(102, "Li");
+
         // 新建AtomicReference对象，初始化它的值为p1对象
         AtomicStampedReference ar = new AtomicStampedReference(p1, 0);
         // 通过CAS设置ar。如果ar的值为p1的话，则将其设置为p2。
